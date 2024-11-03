87077ea2ff8bef5ad9531580e0a2c04f5b8e6a21
ykmxxi <ykmxxi97@gmail.com>
2024-11-03 13:37:48 +0900

    refactor: 결과 화면 기능별 메서드 분리

--- src/main/java/lotto/presentation/view/ResultView.java
+++ src/main/java/lotto/presentation/view/ResultView.java
@@ -1,6 +1,7 @@
 package lotto.presentation.view;
 
 import java.text.NumberFormat;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,41 +12,94 @@ import lotto.service.dto.WinningResult;
 
 public class ResultView {
 
-    public void printLottoPurchaseResult(LottoPurchaseResponse lottoPurchaseResponse) {
-        System.out.println();
-        System.out.printf("%d개를 구매했습니다.", lottoPurchaseResponse.lottoTicketsCount());
-        System.out.println();
+    private static final String NEW_LINE = System.lineSeparator();
 
-        lottoPurchaseResponse.lottoNumbers()
-                .forEach((lottoNumber) -> {
-                    System.out.println(lottoNumber.stream()
-                            .collect(Collectors.joining(", ", "[", "]"))
-                    );
-                });
-        System.out.println();
+    public void printLottoPurchaseResult(final LottoPurchaseResponse lottoPurchaseResponse) {
+        StringBuilder builder = new StringBuilder();
+
+        appendPurchaseCountMessage(builder, formatPurchaseCountMessage(lottoPurchaseResponse.lottoTicketsCount()));
+        appendLottoNumberMessage(builder, lottoPurchaseResponse.lottoNumbers());
+
+        System.out.println(builder);
     }
 
     public void printDrawWinningResult(final DrawWinningResponse drawWinningResponse) {
-        System.out.println();
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        StringBuilder builder = new StringBuilder();
+
+        appendDrawWinningResultGuide(builder);
+        appendWinningResultStatistics(builder, drawWinningResponse);
+        appendRateOfReturn(builder, drawWinningResponse.rateOfReturn());
+
+        System.out.println(builder);
+    }
+
+    private void appendPurchaseCountMessage(final StringBuilder builder, final String purchaseCountMessage) {
+        builder.append(NEW_LINE)
+                .append(purchaseCountMessage)
+                .append(NEW_LINE);
+    }
+
+    private void appendLottoNumberMessage(final StringBuilder builder, final List<List<String>> lottoNumbers) {
+        lottoNumbers.forEach((lottoNumber) -> {
+            builder.append(formatLottoNumberMessage(lottoNumber))
+                    .append(NEW_LINE);
+        });
+    }
+
+    private void appendDrawWinningResultGuide(final StringBuilder builder) {
+        builder.append(NEW_LINE)
+                .append("당첨 통계")
+                .append(NEW_LINE)
+                .append("---")
+                .append(NEW_LINE);
+    }
+
+    private void appendWinningResultStatistics(
+            final StringBuilder builder,
+            final DrawWinningResponse drawWinningResponse
+    ) {
         Map<WinningResult, Long> winningResultStatistics = drawWinningResponse.winningResultStatistics();
         for (WinningResult winningResult : winningResultStatistics.keySet()) {
-            NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.KOREA);
-            String prefix;
-            if (winningResult.prizeMoney() == 30_000_000L) {
-                prefix = String.format("%d개 일치, 보너스 볼 일치 (%s원)", winningResult.matchingCount(),
-                        numberFormat.format(winningResult.prizeMoney()));
-            } else {
-                prefix = String.format("%d개 일치 (%s원)", winningResult.matchingCount(),
-                        numberFormat.format(winningResult.prizeMoney()));
-            }
-            String suffix = String.join("", Long.toString(winningResultStatistics.get(winningResult)), "개");
-            System.out.println(String.join(" - ", prefix, suffix));
-        }
-        String rateOfReturn = String.join("", drawWinningResponse.rateOfReturn(), "%");
-        System.out.printf("총 수익률은 %s입니다.", rateOfReturn);
+            String prefix = formatPrefix(winningResult);
 
+            long winningCount = winningResultStatistics.get(winningResult);
+            String suffix = formatSuffix(Long.toString(winningCount));
+            builder.append(String.join(" - ", prefix, suffix))
+                    .append(NEW_LINE);
+        }
+    }
+
+    private void appendRateOfReturn(final StringBuilder builder, final String rateOfReturn) {
+        String rateOfReturnMessage = formatRateOfReturnMessage(rateOfReturn);
+        builder.append(rateOfReturnMessage);
+    }
+
+    private String formatPurchaseCountMessage(final int lottoTicketsCount) {
+        return String.join("", lottoTicketsCount + "개를 구매했습니다.");
+    }
+
+    private String formatLottoNumberMessage(final List<String> lottoNumber) {
+        return lottoNumber.stream()
+                .collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    private String formatPrefix(final WinningResult winningResult) {
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.KOREA);
+        if (winningResult.hasBonusNumber()) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%s원)", winningResult.matchingCount(),
+                    numberFormat.format(winningResult.prizeMoney()));
+        }
+        return String.format("%d개 일치 (%s원)", winningResult.matchingCount(),
+                numberFormat.format(winningResult.prizeMoney()));
+    }
+
+    private String formatSuffix(final String winningResultStatistics) {
+        return String.join("", winningResultStatistics, "개");
+    }
+
+    private String formatRateOfReturnMessage(final String rateOfReturn) {
+        String rateOfReturnPercentage = String.join("", rateOfReturn, "%");
+        return String.format("총 수익률은 %s입니다.", rateOfReturnPercentage);
     }
 
 }
