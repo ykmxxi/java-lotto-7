c1927e115492cbd20b5b37970c9e55e0bf2df615
ykmxxi <ykmxxi97@gmail.com>
2024-11-04 14:53:26 +0900

    test: 로또 매칭 개수 계산과 포함 여부 기능 테스트

--- src/test/java/lotto/domain/lotto/LottoTest.java
+++ src/test/java/lotto/domain/lotto/LottoTest.java
@@ -1,13 +1,26 @@
 package lotto.domain.lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
+
+    private Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = Lotto.issue(List.of(1, 2, 3, 4, 5, 6));
+    }
 
     @DisplayName("로또 번호가 6개가 아니면 예외가 발생한다.")
     @Test
@@ -31,6 +44,37 @@ class LottoTest {
         assertThatThrownBy(() -> Lotto.issue(List.of(0, 1, 2, 3, 45, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContainingAll("로또 번호", "1", "45");
+    }
+
+    @DisplayName("로또는 다른 로또와 비교해 일치하는 숫자의 개수를 계산할 수 있다.")
+    @MethodSource(value = "provideOtherLottoAndExpectedMatchingCount")
+    @ParameterizedTest
+    void 다른_로또와_일치하는_로또_번호_개수_계산(Lotto other, int expectedMatchingCount) {
+        assertThat(lotto.calculateMatchingCount(other)).isEqualTo(expectedMatchingCount);
+    }
+
+    @DisplayName("로또는 주어진 번호의 포함 여부를 알려준다.")
+    @MethodSource(value = "provideLottoBallAndExpectedHas")
+    @ParameterizedTest
+    void 로또_번호_포함_여부(LottoBall lottoBall, boolean expectedHas) {
+        assertThat(lotto.has(lottoBall)).isEqualTo(expectedHas);
+    }
+
+    static Stream<Arguments> provideOtherLottoAndExpectedMatchingCount() {
+        return Stream.of(
+                Arguments.of(Lotto.issue(List.of(1, 2, 3, 4, 5, 6)), 6),
+                Arguments.of(Lotto.issue(List.of(1, 2, 3, 4, 5, 10)), 5),
+                Arguments.of(Lotto.issue(List.of(1, 2, 3, 4, 10, 11)), 4),
+                Arguments.of(Lotto.issue(List.of(1, 2, 3, 10, 11, 12)), 3),
+                Arguments.of(Lotto.issue(List.of(10, 11, 12, 13, 14, 15)), 0)
+        );
+    }
+
+    static Stream<Arguments> provideLottoBallAndExpectedHas() {
+        return Stream.of(
+                Arguments.of(LottoBall.draw(1), true),
+                Arguments.of(LottoBall.draw(10), false)
+        );
     }
 
 }
